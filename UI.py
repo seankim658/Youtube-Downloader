befa596bdd716f627fe75722e2400aec91e6d786
skim658 <33474168+seankim658@users.noreply.github.com>
2022-07-26 22:50:10 -0400

    adding some comments

--- UI.py
+++ UI.py
@@ -6,52 +6,70 @@ from API import API
 
 class App(tk.Frame):
     def __init__(self):
+        ''' App constructor 
+        '''
         super(App, self).__init__()
-        self.widgets = []
+        self.main_widgets = []
         self.result_widgets = []
         self.grid()
         self.create_widgets()
         self.first_run = True 
         self.api = API()
+        # default download location is main drive root folder 
         self.download_destination_string = 'C:\\'
     
     def create_widgets(self):
+        ''' Creates the home screen widgets 
+        '''
+        # first url entry bar 
         url = tk.Entry(self, font = ('Courier New', 12), width = 50)
         url.grid()
-        self.widgets.append(url)
+        self.main_widgets.append(url)
+        # download video button 
         self.download_video_button = tk.Button(self, text = 'Download Video', command = lambda : self.download('video'))
         self.download_video_button.grid(row = 0, column = 1)
+        # download audio button 
         self.download_audio_button = tk.Button(self, text = 'Download Audio', command = lambda : self.download('audio'))
         self.download_audio_button.grid(row = 0, column = 2)
+        # set download destination 
         self.download_destination_button = tk.Button(self, text = 'Download Destination', command = lambda : self.download_destination())
         self.download_destination_button.grid(row = 0, column = 3)
+        # add download button 
         self.clone_button = tk.Button(self, text='Add Download', command = lambda : self.clone())
         self.clone_button.grid(row = 0, column = 4)
     
     def download_destination(self):
+        ''' Set the download destination 
+        '''
         self.download_destination_string = filedialog.askdirectory()
         print(self.download_destination_string)
     
     def clone(self):
+        ''' Add new download entry 
+        '''
         if self.first_run == True:
+            # if adding a second entry bar create a remove button 
             self.remove_button = tk.Button(self, text = 'Remove Download', command = lambda : self.remove())
             self.remove_button.grid(row = 0, column = 5)
             self.first_run = False
+        # create the new entry bar 
         widget = tk.Entry(self, font = ('Courier New', 12), width = 50)
-        widget.grid()
-        self.widgets.append(widget)
+        widget.grid(row = len(self.main_widgets), column = 0)
+        self.main_widgets.append(widget)
     
     def remove(self):
-        if len(self.widgets) == 1:
+        ''' Remove excess download entries 
+        '''
+        if len(self.main_widgets) == 2:
             self.remove_button.destroy()
             self.first_run = True
-        if len(self.widgets) >= 1:
-            widget = self.widgets.pop(len(self.widgets) - 1)
+        if len(self.main_widgets) >= 1:
+            widget = self.main_widgets.pop(len(self.main_widgets) - 1)
             widget.destroy()
 
     def download(self, mode):
         videos = []
-        for widget in self.widgets:
+        for widget in self.main_widgets:
             url = widget.get()
             current_vid = self.api.get_video(url)
             self.show_results(current_vid)
@@ -68,7 +86,9 @@ class App(tk.Frame):
             self.result_widgets.append(result) 
 
     def clear_text(self):
-        for widget in self.widgets:
+        ''' Clear the entry bars 
+        '''
+        for widget in self.main_widgets:
             widget.delete(0, 'end')
         
 
